59d9235a9607a789b8cdfae8ab6a8e75f920cb4e
Augustas Mikulenas <augustmik@gmail.com>
2022-03-28 12:26:22 +0300

    added jupyter lab code into git

---
+++ CardFinder env versions/card_finder(v2).py
@@ -0,0 +1,123 @@
+import sys
+from contextlib import closing
+
+import numpy as np
+from io import StringIO
+
+from gym import utils
+from gym.envs.toy_text import discrete
+
+LEFT = 0
+DOWN = 1
+RIGHT = 2
+UP = 3
+terminal_output = open(1, 'w')
+
+MAPS = {
+    "4x4": ["SFFF", "FHFH", "FFFH", "HFFG"],
+    "8x8": [
+        "SFFFFFFF",
+        "FFFFFFFF",
+        "FFFHFFFF",
+        "FFFFFHFF",
+        "FFFHFFFF",
+        "FHHFFFHF",
+        "FHFFHFHF",
+        "FFFHFFFG",
+    ],
+}
+
+
+def generate_random_map(size=9, p=0.50):
+    """Generuoja atsitiktinį tinkamą žemėlapį
+    :param size: kiekvienos žemėlapio kraštinės dydis
+    :param p: tikimybė, kad langelis ant kurio agentui galima lipti
+    """
+    valid = False
+    while not valid:
+        p = min(1, p)
+        res = np.random.choice(["F", "H"], (size, size), p=[p, 1 - p])
+        res[0][0] = "S"
+        res[-1][-1] = "G"
+        valid = True #is_valid(res)
+    return ["".join(x) for x in res]
+
+
+class CardFinderEnv(discrete.DiscreteEnv):
+    metadata = {"render.modes": ["human", "ansi"]}
+
+    def __init__(self, desc=None, map_name="4x4"):
+        if desc is None and map_name is None:
+            desc = generate_random_map()
+        elif desc is None:
+            desc = generate_random_map() #MAPS[map_name]
+        self.desc = desc = np.asarray(desc, dtype="c")
+        self.nrow, self.ncol = nrow, ncol = desc.shape
+        self.reward_range = (-10, 20)
+
+        nA = 4
+        nS = nrow * ncol
+
+        isd = np.array(desc == b"S").astype("float64").ravel()
+        isd /= isd.sum()
+
+        P = {s: {a: [] for a in range(nA)} for s in range(nS)}
+        
+        def to_s(row, col):
+            return row * ncol + col
+
+        def inc(row, col, a):
+            if a == LEFT:
+                col = max(col - 1, 0)
+            elif a == DOWN:
+                row = min(row + 1, nrow - 1)
+            elif a == RIGHT:
+                col = min(col + 1, ncol - 1)
+            elif a == UP:
+                row = max(row - 1, 0)
+            return (row, col)
+
+        def update_probability_matrix(row, col, action):
+            newrow, newcol = inc(row, col, action)
+            newstate = to_s(newrow, newcol)
+            newletter = desc[newrow, newcol]
+            done = bytes(newletter) in b"G"
+            if newletter == b"G":
+                reward = 20
+            elif newletter == b"H":
+                reward = -10
+            else: 
+                reward = -1
+            return newstate, reward, done
+
+        for row in range(nrow):
+            for col in range(ncol):
+                s = to_s(row, col)
+                for a in range(4):
+                    li = P[s][a]
+                    letter = desc[row, col]
+                    if letter in b"G":
+                        li.append((1, s, 20, True))
+                    else:
+                        li.append((1, *update_probability_matrix(row, col, a)))
+
+        super(CardFinderEnv, self).__init__(nS, nA, P, isd)
+
+    def render(self, mode="human"):
+        outfile = StringIO() if mode == "ansi" else sys.stdout
+
+        row, col = self.s // self.ncol, self.s % self.ncol
+        desc = self.desc.tolist()
+        desc = [[c.decode("utf-8") for c in line] for line in desc]
+        desc[row][col] = utils.colorize(desc[row][col], "red", highlight=True)
+        if self.lastaction is not None:
+            outfile.write(
+                "  ({})\n".format(["Left", "Down", "Right", "Up"][self.lastaction])
+            )
+        else:
+            outfile.write("\n")
+        outfile.write("\n".join("".join(line) for line in desc) + "\n")
+
+        if mode != "human":
+            with closing(outfile):
+                return outfile.getvalue()
